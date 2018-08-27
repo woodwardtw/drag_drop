f3f38eba383ff69287b2306c7396778ef32b1dd6
Tom Woodward <twwoodward@me.com>
2018-08-26 22:39:13 -0400

    multiple destinations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 let left = 'left-copy-1tomany';
 let right = 'right-copy-1tomany';
+let rightb = 'right-copy-1tomanyb';
 
-dragula([document.getElementById(left), document.getElementById(right)], {
+dragula([document.getElementById(left), document.getElementById(right), document.getElementById(rightb)], {
   copy: function (el, source) {
     return source === document.getElementById(left)
   },
